01330e27d7511f57ddbb326cb68a7d67915b26d5
Ali Duman <alidumanyp@gmail.com>
2023-05-05 04:59:05 +0300

    01

---
+++ app/src/main/java/com/aliduman/savemyage/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.aliduman.savemyage
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var preferences : SharedPreferences
+    private lateinit var age_button : Button
+    private lateinit var age_text : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        age_button = findViewById(R.id.save_button)
+        age_text = findViewById(R.id.age_text)
+
+        preferences = this.getSharedPreferences("com.aliduman.savemyage", Context.MODE_PRIVATE)
+
+        var currentAge = preferences.getInt("age",-1)
+
+        if (currentAge != -1) {
+            age_text.hint = currentAge.toString()
+
+        } else {
+
+            age_button.setOnClickListener {
+
+                var ageInput =  Integer.valueOf(age_text.text.toString())
+                preferences.edit().putInt("age", ageInput).apply()
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
